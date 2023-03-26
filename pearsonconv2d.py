651be9d27f61b799c6609d72b58c8e74f248910e
Louis Fabrice Tshimanga <louisfabrice13@gmail.com>
2023-03-26 19:20:06 +0200

    new functions, working decently, poorly commented; must implement cosine option and test strides, dilations and groups

--- pearsonconv2d.py
+++ pearsonconv2d.py
@@ -1,81 +1,69 @@
-import tensorflow as tf
+@tf.function
+def pearsoncoeff(windows_centered_flattened, weights_centered_flattened):
+  """This function assumes weights and windows are already mean centered.
+  windows_centered of shape (batch, n_windows_h, n_windows_w, ws1*ws2*n_ch_in)
+  weights_centered of shape (window_size1 * window_size2 * n_ch_in, n_ch_out)
+  Outputs are 'flat' pearson coefficients"""
+  # Numerator is the dot product of each window and the kernel, i.e. sum of products of same-index values
+  # i.e. matmul with result (n_windows, n_ch_out)
+  numerator = tf.einsum("ijkh,hl->ijkl", windows_centered_flattened, weights_centered_flattened)
+  # Denominator is the product of l2-norms (euclidean norms, vector magnitudes)
+  # print(weights_centered_flattened.shape)
+  # windows_centered_flattened = tf.reshape(windows_centered_flattened, shape=(windows_centered_flattened.shape[0],
+  #                                                                            ))
+  denominator = tf.math.multiply(tf.norm(windows_centered_flattened, axis=3, keepdims=True)+1e-12,
+                                 tf.norm(weights_centered_flattened, axis=0, keepdims=True)+1e-12,
+                                 )
+  # denominator = tf.einsum("ijkh,hl->ijkl", windows_centered_flattened, weights_centered_flattened)
+
+  pearson_coeff_flat = tf.math.divide(numerator, denominator)
+  return pearson_coeff_flat
 
 @tf.function
-def depthwise_samplewise_pearsonconv2d(x, W, strides=[1, 1, 1, 1], rates=[1, 1, 1, 1], padding_val='SAME'):
-    """
-    Input x has shape (Height, Width, 1)
-    Kernel W has shape (W_height, W_width)
-    """
-    W = tf.expand_dims(W, axis=-1)
-    W = tf.expand_dims(W, axis=0)
-    x = tf.expand_dims(x, axis=0)
-    # Flatten kernel
-    window_size1 = W.get_shape().as_list()[1]
-    window_size2 = W.get_shape().as_list()[2]
-    W_flat = tf.reshape(W, (1,1,1,window_size1*window_size2))
-    # Center_kernel
-    W_means =  tf.reduce_mean(W_flat, axis=[3], keepdims=True)
-    W_centered = tf.math.subtract(W_flat, W_means)
-    W_norm = tf.norm(W_centered, axis=3, keepdims=True) + 1e-12
-    
-    # Split the input tensor into windows
-    windows = tf.image.extract_patches(x, [1, window_size1, window_size2, 1], strides=[1, 1, 1, 1], rates=[1, 1, 1, 1], padding=padding_val)
-    
-    # Compute the mean of each window
-    window_means = tf.reduce_mean(windows, axis=[3], keepdims=True)
+def sample_pearsoncoeff(x_sample, kernel, padding_val="SAME", strides=(1,1), rates=(1,1)):
+  """This function takes a 2-D matrix (a single channel from a tensor (1xHxWxC)), 
+  divides it in windows that are flatten, and mean-centers them.
+  Kernel weights are assumed already mean centered.
+  Outputs are 2-D matrix of the pearson coefficients between windows and kernel"""
+  if padding_val=="same":
+    padding_val=="SAME"
+  elif padding_val=="valid":
+    padding_val=="VALID"
 
-    # Subtract the mean from each window
-    windows_centered = tf.math.subtract(windows, window_means) # the windows are already flattened, the means are broadcast
-    
-    # Compute the norms of centered windows
-    window_norms = tf.norm(windows_centered, axis=3, keepdims=True) + 1e-12
-    
-    # Perform the dot product of centered quantities
-    pearson = tf.reduce_sum(tf.math.multiply(windows_centered, W_centered), axis=3, keepdims=True)
-    
-    # Divide the result by the product of magnitudes
-    denominator = tf.math.multiply(window_norms, W_norm)
-    pearson = tf.math.divide(pearson, denominator)
-    return pearson[0]
+  stride1, stride2 = strides
+  rate1, rate2 = rates
+  
+  window_size1 = kernel.get_shape().as_list()[0]
+  window_size2 = kernel.get_shape().as_list()[1]
+  n_ch_in = kernel.get_shape().as_list()[2]
+  n_ch_out = kernel.get_shape().as_list()[3]
+  weights_flattened = tf.reshape(kernel, (window_size1*window_size2*n_ch_in, n_ch_out))
+  
+  # Divide input in flat windows
+  x_windows = tf.image.extract_patches(x_sample, [1, window_size1, window_size2, 1], strides=[1, stride1, stride2, 1], rates=[1, rate1, rate2, 1], padding=padding_val)
+  # x_windows of shape (batch_size, index_hs, index_ws, window_size1*window_size2*n_ch_in)
 
-@tf.function
-def samplewise_pearsonconv2d(x, W, strides=[1, 1, 1, 1], rates=[1, 1, 1, 1], padding_val='SAME'):
-    if padding_val=="same":
-        padding_val = "SAME"
-    if padding_val=="valid":
-        padding_val = "VALID"
-    # W.shape=(k_size, k_size, in, out)
-    # for every output channel we are applying a filter (k_size, k_size, in, 1) whose output is sum_reduced along the input channel dim
-    feature_maps = tf.TensorArray(tf.float32, size=0, dynamic_size=True)
-    for channel_out in tf.range(W.get_shape().as_list()[3]):
-        W_filter = W[:,:,:,channel_out]
-        filter_response = tf.TensorArray(tf.float32, size=0, dynamic_size=True)
-        for channel_in in tf.range(W.get_shape().as_list()[2]):
-            W_slice = W_filter[:,:,channel_in]
-            x_slice = tf.expand_dims(x[:,:,channel_in], -1)
-            out_slice = depthwise_samplewise_pearsonconv2d(x_slice, W_slice, strides=strides, rates=rates, padding_val=padding_val)
-            filter_response = filter_response.write(channel_in, out_slice)
-        # end of inputchannel iterations, time to output single response for W_filter
-        filter_response = tf.math.reduce_sum(filter_response.stack(), axis=0, keepdims=False)[:,:,0]
-        # time to concatenate this response with others
-        feature_maps = feature_maps.write(channel_out, filter_response)
-    # end of outputchannel iterations, time to concatenate filter_responses into one long tensor
-    feature_maps = feature_maps.stack()
-    # (None, outsize1, outsize2, 1) needs to be reshaped
-    feature_maps = tf.transpose(feature_maps, perm=[2,1,0])
-    # feature_maps = tf.squeeze(feature_maps, axis=0)
-    return feature_maps
+  # Mean-center windows
+  x_windows_means = tf.reduce_mean(x_windows, axis=[3], keepdims=True)
+  x_windows_centered = tf.math.subtract(x_windows, x_windows_means)
+  # print(x_windows_centered.shape)
+  # Compute correlations between windows and kernel that apply
+  # x_windows_centered_flattened = tf.reshape(x_windows_centered, 
+  #                                           shape=[x_windows_centered.shape[0], 
+  #                                                  x_windows_centered.shape[1]*x_windows_centered.shape[2], x_windows_centered.shape[3]])
+  y_windows_pearson = pearsoncoeff(x_windows_centered, weights_flattened) # shape (n_windows, n_ch_out)
 
-@tf.function
-def batch_pearsonconv2d(x, W, strides=[1, 1, 1, 1], rates=[1, 1, 1, 1], padding_val="VALID"):
-    batch_maps = tf.TensorArray(tf.float32, size=0, dynamic_size=True)
-    for sample in tf.range(x.get_shape().as_list()[0]):
-        # x_sample = tf.expand_dims(x[sample,:,:,:], 0)
-        x_sample = x[sample,:,:,:]
-        feat_maps = samplewise_pearsonconv2d(x_sample, W, strides=strides, rates=rates, padding_val=padding_val)
-        # feat_maps = tf.expand_dims(feat_maps, -1)
-        feat_maps = tf.expand_dims(feat_maps, 0)
-        # feat_maps = tf.reshape(feat_maps, [feat_maps.get_shape().as_list()[0],feat_maps.get_shape().as_list()[1],1])
-        batch_maps = batch_maps.write(sample, feat_maps)
-    batch_maps = batch_maps.concat()
-    return batch_maps
+  # Reshape correlations with abt (x.shape[0], ~x.shape[1], ~x.shape[2], x.shape[3])
+  y_shape1 = x_windows.get_shape().as_list()[1]
+  y_shape2 = x_windows.get_shape().as_list()[2]
+  y_pearson = tf.reshape(y_windows_pearson, [-1, y_shape1, y_shape2, n_ch_out])
+  return y_pearson
+
+class StandardizedConv2DWithCall(tf.keras.layers.Conv2D):
+    def call(self, inputs):
+        batch_size = tf.shape(inputs)[0]
+        # inputs.set_shape(shape=(batch_size, inputs.shape[1], inputs.shape[2], inputs.shape[3]))
+        kernel_centered = self.kernel - tf.math.reduce_mean(self.kernel, axis=[0,1,2], keepdims=True)
+        outputs = sample_pearsoncoeff(inputs, kernel_centered)
+        # outputs.set_shape(shape=(batch_size, outputs.shape[1], outputs.shape[2], outputs.shape[3]))
+        return outputs
